5bc23e4b31b6b2b7f43458f90b8d864dfbb76fd6
Hamdy-Mohamed <121273938+Hamdy-Mohamed@users.noreply.github.com>
2023-04-26 04:41:47 +0200

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,19 @@
-function removetitle() {
+function removeTitleAndCalcDays() {
   let title = document.querySelector("h1");
   let calc_input = document.querySelector(".calc_input");
   let dayes = document.querySelector(".dayes");
+  let icons = document.querySelector(".links");
   title.addEventListener("click", () => {
     title.style.display = "none";
     calc_input.style.display = "flex";
     calc_input.style.margin = "20rem auto";
     dayes.style.display = "flex";
+    icons.style.display = "flex";
     calc_input.addEventListener("change", function () {
       calc_age = calc_input.value * 365;
       dayes.innerHTML = `Your Days old is: ${calc_age} `;
-      if (calc_age == "-365") {
+
+      if (calc_age <= "-365") {
         alert(
           "Well, you're not even born yet😂🤣🤰, Try writing the age of someone who's here with us"
         );
@@ -19,4 +22,25 @@ function removetitle() {
     });
   });
 }
-removetitle();
+removeTitleAndCalcDays();
+
+function icons() {
+  let facebook_icon = document.querySelector(".links :nth-child(1)");
+  let twitter_icon = document.querySelector(".links :nth-child(2)");
+  let github_icon = document.querySelector(".links :nth-child(3)");
+  let linkedin_icon = document.querySelector(".links :nth-child(4)");
+  facebook_icon.onclick = function () {
+    window.open("https://www.facebook.com/hamdy.elgokar.5");
+  };
+  twitter_icon.onclick = function () {
+    window.open("https://twitter.com/HamdyMohamedd1");
+  };
+  github_icon.onclick = function () {
+    window.open("https://github.com/Hamdy-Mohamed");
+  };
+  linkedin_icon.onclick = function () {
+    window.open("https://www.linkedin.com/in/hamdy-abdal-majeed-b67b32246/");
+  };
+}
+
+icons();
